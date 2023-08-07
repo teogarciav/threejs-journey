96f213d4a309258e3d03924057c1c2a129ac5b13
Teo <teogarciavilla@gmail.com>
2023-08-07 20:31:17 -0300

    Animating cube rotation with 60fps cap using Date.now() - time

--- 6-animations/src/script.js
+++ 6-animations/src/script.js
@@ -28,4 +28,26 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Time
+let time = Date.now()
+
+// Animation
+const tick = () => {
+
+    // Time
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+    //console.log(deltaTime)
+    //console.log(time)
+
+    //Update Objects
+    mesh.rotation.y += 0.001 * deltaTime
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
