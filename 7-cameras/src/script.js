f334821a1120f6ee3b4ac7957ceef10d266a4f8c
Teo <teogarciavilla@gmail.com>
2023-08-07 22:11:42 -0300

    Orbit (draggable) controls (7)

--- 7-cameras/src/script.js
+++ 7-cameras/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+ 
 // Cursor
 const cursor = {
     x:0,
@@ -43,6 +44,13 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -60,11 +68,14 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     //Update Camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
 
+    //Update Controls
+    controls.update()
+    
     // Render
     renderer.render(scene, camera)
 
