337408d463faa315d4fe38bbc5aaeb1069e98dc9
Teo <teogarciavilla@gmail.com>
2023-08-15 19:15:33 -0300

    Added vercel builds and changed Hello Text from 14-golive

--- 14-live/src/script.js
+++ 14-live/src/script.js
@@ -38,7 +38,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Hello Teo.js',
             {
                 font: font,
                 size: 0.5,
